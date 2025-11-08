f289d1c6b066b74dba2be88a353b7631d4e9ecd7
AntonioSkara132 <conio1240@gmail.com>
2025-11-08 15:14:37 +0000

    Boneback of data-splittter

--- src/data-splitter/data-splitter-node.cpp
+++ src/data-splitter/data-splitter-node.cpp
@@ -1,7 +1,57 @@
+#pragma once
+
+#include "defines.h"
 #include "rclcpp/rclcpp.hpp"
 #include "utils.h"
 
 using std::placeholders::_1;
+
+/* CODE SNIPPET FOR BACK ANGLE IGNORING
+
+void Game::Apply_Avoidence(unsigned int boid_index)
+{
+    // sum of all avoidence forces that each boid in swarm applies to boid in boid_index
+    Vector2 avoidence_strength = (Vector2){0.0f,0.0f};
+
+    for (unsigned int i = 0; i < last_number_of_boids; i++)
+    {
+        if (i == boid_index)
+            continue;
+
+        // order is not inverse of this, vector direction is handeled by Boid::Update_Velocity
+        Vector2 delta;
+        delta.x = boids[i].position.x - boids[boid_index].position.x;
+        delta.y = boids[i].position.y - boids[boid_index].position.y;
+
+
+        // is within radius? 
+        // is within not cropped out part of the circle relative to current direction?
+        if ((delta.x * delta.x + delta.y * delta.y) <= (avoidence_visual_field_radius * avoidence_visual_field_radius) &&                                 
+            std::abs(Vector2Angle(boids[boid_index].velocity, delta)) <= side_angel_view)  
+        {
+            avoidence_strength.x += delta.x / (delta.x * delta.x + delta.y * delta.y);
+            avoidence_strength.y += delta.y / (delta.x * delta.x + delta.y * delta.y);
+        }
+    }
+
+    boids[boid_index].acceleration_avoidence.x += avoidence_strength.x * avoidence_factor;
+    boids[boid_index].acceleration_avoidence.y += avoidence_strength.y * avoidence_factor;
+}
+
+
+BITAN JE DIO
+    std::abs(Vector2Angle(boids[boid_index].velocity, delta)) <= side_angel_vie
+    float Vector2Angle(Vector2 v1, Vector2 v2)
+    {
+        float result = atan2f(v2.y - v1.y, v2.x - v1.x)*(180.0f/PI);
+
+        if (result < 0) result += 360.0f;
+
+        return result;
+    }
+TO SAM TAKO KORISTIO JER JE .VELOCITY IMAO INFORMATION I SMJERU, ALI SADA TO IMAMO DIREKTNO IZ KUTA,
+DODUSE NEZ AKO BI NAM BILO MUDRIJE ISKORISTITI TO ILI OVO
+*/
 
 class Data_Splitter_Node : public rclcpp::Node
 {
@@ -10,33 +60,33 @@ public:
     {
         boit_fov_ = this->declare_parameter<float>("boit_fov", 0.0f);
         boit_vision_range_ = this->declare_parameter<float>("boit_vision_range", 0.0f);
-        map_sub_ = this->create_subscription<Msg_Map>("/map", 10, std::bind(&Data_Splitter_Node::Map_Callback, this))
+        num_boids_ = this->declare_parameter<uint>("boid_number", 1);
 
-        uint highest_robot_id = 0;
-        {
-            auto topics = get_topic_names_and_types();
-            for (const auto& topic : topics)
-            {
-                std::string topic_name = topic.first;
-                if (string_contains(topic_name, "robot_") && string_contains(topic_name, "/odom"))
-                {
-                    uint robot_id = parse_number_from_string(topic_name);
-                    if (robot_id > highest_robot_id)
-                        highest_robot_id = robot_id;                
-                }
-            }
-        }
+        map_sub_ = this->create_subscription<Msg_Map>("/map", 10, std::bind(&Data_Splitter_Node::Map_Callback, this, _1));
+        timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
         
+        uint[] robot_ids;
+        for (int i = 0; i < num_boids_; ++i) {
+            robot_ids.push_back(i);
+        }
+
+        // generiranje topica za subscribere
+        std::vector<std::string> odom_topics;
+        std::vector<std::string> cmd_vel_topics;
+
+        for (uint id : robot_ids) {
+            odom_topics.push_back("/" + std::to_string(id) + "/odom");
+            cmd_vel_topics.push_back("/" + std::to_string(id) + "/cmd_vel");
+        }
+
         for (uint i = 0; i < highest_robot_id; ++i)
         {
-            std::string robot_odom_topic = "/robot_" + std::to_string(i) + "/odom";
-            std::string robot_twist_topic = "/robot_" + std::to_string(i) + "/cmd_vel";
-        
             Subscription_Odom sub = this->create_subscription<Msg_Odom>(
-                    robot_odom_topic, 
+                    odom_topics[i], 
                     10, 
                     [this, i](const Msg_Odom::SharedPtr msg) {
                         this->Subscription_Odom_Callback(msg, i);
+
                     }
                 ); 
             subs.push_back(sub);
@@ -51,18 +101,29 @@ public:
     }
 private:    
     
-    rclcpp::Subscription<Msg_Map> map_sub_;
+    Subscription_Map map_sub_;
 
     float boit_fov_;
     float boit_vision_range_;
+    float num_boids_;
+
+    std::vector<Publisher_Twist> pubs;
+    std::vector<Subscription_Odom> subs;
     
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&Data_Splitter_Node::odom_msg_callback, this));
+
     std::vector<Publisher_Twist> pubs;
     std::vector<Subscription_Odom> subs;
 
     Msg_Map map_;
 
-    void Subscription_Odom_Callback(const Msg_Odom::SharedPtr odom, uint id);
-    void Data_Splitter_Node::Map_Callback(const Msg_Map::SharedPtr map){
+    // publisher koji ce publishati poruke o obstaclima i susjedima
+    void Subscription_Odom_Callback(const Msg_Odom::SharedPtr odom, uint id){
+        
+    }
+    
+    void Map_Callback(const Msg_Map::SharedPtr map){
         map_ = *map;
     }
 
@@ -111,6 +172,5 @@ private:
 
         return closest_obstacle;
     }
-
 
 };
