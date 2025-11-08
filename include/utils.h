2a7d5950eaaa4ce8e7b80c3d724243a9f18f4ae4
Dino <leapersedge@gmail.com>
2025-11-08 13:40:09 +0100

    turned boit controller into a single boit controller

--- include/utils.h
+++ include/utils.h
@@ -4,13 +4,6 @@
 #include "vector2.hpp"
 #include "defines.h"
 
-struct Boit
-{
-    bool initialized = false;
-    float last_rotation = 0.0f; 
-    clock_t last_time = 0;
-};
-
 float squared_euclidan_norm(Vector2 vec);
 
 float p_controller_update(float reference, float state, float kp);
