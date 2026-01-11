be6551912b16bd909f68470872c628313e06f64d
cicoita <itapoklepovic1@gmail.com>
2026-01-11 16:48:03 +0100

    updated bidder.py - winner and task callback

--- scripts/bidder.py
+++ scripts/bidder.py
@@ -27,9 +27,12 @@ class Bidder(Node):
             self.get_parameter('start_y').value
         ]
 
-        self.speed = 1 # cost of task will be calculated using speed and distance to point and durtion of task
+        self.speed = 1  # cost of task will be calculated using speed and distance to point and durtion of task
+        # mozda moze ovako ostat stin da su svi iste brzine? nije bitno
         self.my_schedule = []
+        self.tasks = dict()
         self.last_processed_round = -1
+        self.active_bid = None
 
         # 2. Publishers
         self.bid_publisher = self.create_publisher(Bid, 'bids', 10)
@@ -44,7 +47,7 @@ class Bidder(Node):
     def calculate_bid_value(self, task):
         """Logic to determine the cost for a task"""
         # TODO: Implement calculation
-        return 0
+        return (0, placement_in_schedule, start)
 
     def task_callback(self, msg):
         """Triggered when the Auctioneer publishes tasks"""
@@ -52,22 +55,52 @@ class Bidder(Node):
             return
 
         self.last_processed_round = msg.auction_round
+        lowest_cost = float("inf")
+        bid = None
 
-        # TODO: evaluate all tasks
-        # TODO: pick best task
-        # TODO: make a bid
+        # evaluate all tasks and pick the best one
+        for task in msg.tasks:
+            cost, placement_in_schedule, start = self.calculate_bid_value(task)
+            if cost < lowest_cost:
+                lowest_cost = cost
+                bid = (task, placement_in_schedule, start)
+
+        # make a bid
+        task, placement_in_schedule, start = bid
 
         best_bid = Bid()
-        best_bid.bidder_id = self.robot_id
         best_bid.auction_round = msg.auction_round
+        best_bid.bidder_id = self.robot_id
+        best_bid.task_id = task.id
+        best_bid.bid = lowest_cost
+        best_bid.task_start = start
 
-        # self.bid_publisher.publish(best_bid)
+        self.active_bid = (task, placement_in_schedule, best_bid)
+        self.bid_publisher.publish(best_bid)
 
-    def winner_callback(self, msg):
+    def winner_callback(self, winning_bid):
         """Triggered when the Auctioneer announces a winner."""
-        # TODO: check if youre the winner
-        # TODO: if winner place task in schedule
-        # TODO: if task with same id in schedule update it
+        if winning_bid.auction_round != self.last_processed_round:
+            return
+        # check if you're the winner
+        if winning_bid.bidder_id == self.robot_id:
+            task, placement_in_schedule, bid = self.active_bid
+            # if winner place task in schedule
+            if task.id not in self.tasks:
+                self.tasks[task.id] = task
+                self.my_schedule.insert(placement_in_schedule, task.id)
+            else:
+                self.tasks[task.id].earliest_start = task.earliest_start
+                index = self.my_schedule.index(task.id)
+                # ako updatean task nije zadnji u rasporedu trebaju se pushat ostali ako ih ne stigne obavit na vrijeme
+                if index != len(self.my_schedule) - 1:
+                    next_task_id = self.my_schedule[index + 1]
+                    diff = self.tasks[task.id].earliest_start + self.tasks[task.id].duration - self.tasks[next_task_id].earliest_start
+                    if diff > 0:
+                        for task_id_to_pushback in self.my_schedule[index + 1:]:
+                            self.tasks[task_id_to_pushback].earliest_start += diff
+
+        self.active_bid = None
 
 
 def main(args=None):
