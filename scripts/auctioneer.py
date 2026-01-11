ff0475910b4c2e0d94ae107bd7bb197ff0efce8a
cicoita <itapoklepovic1@gmail.com>
2026-01-11 11:57:40 +0100

    updated auctioneer.py

--- scripts/auctioneer.py
+++ scripts/auctioneer.py
@@ -11,39 +11,44 @@ then permanently adds the tasks to its list of assigned tasks. The auctioneer st
 remaining tasks until all tasks from TF are assigned. """
 
 import rclpy as ros
-import networkx as nx       #onaj library za rad s grafovima
+import networkx as nx  # onaj library za rad s grafovima
 import numpy as np
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
 import os
 import json
 import matplotlib.pyplot as plt
-from vissus_projekt.msg import TaskList, Task
+from vissus_projekt.msg import TaskList, Task, Bid
+
 
 class Auctioneer(Node):
     def __init__(self):
         super().__init__('Auctioneer')
         tasks = self.load_mission_config()
         self.task_graph = self.build_tasks_graph(tasks)
-        self.visualize_task_graph(self.task_graph)
-
-        self.task_publisher = self.create_publisher(
-            TaskList,
-            'task_list',
-            10
-        )
-
         self.free_tasks = list()
+        self.free_tasks_bids = dict()
+        self.expected_bids = self.get_num_robots()
         self.allocated_tasks = list()
+        self.auction_round = 0
+
+        self.task_publisher = self.create_publisher(TaskList, 'auction_task_list', 10)
+
+        self.winner_publisher = self.create_publisher(Bid, 'auction_winner', 10)
+
+        self.bid_listener = self.create_subscription(Bid, 'bids', self.bid_evaluator_callback, 10)
 
         self.publish_free_tasks()
 
+    def get_num_robots(self):
+        return 4  # TODO: forward from launch
 
     def build_tasks_graph(self, tasks_data):
         task_graph = nx.DiGraph()
         for t in tasks_data:
-            # add task nodes
-            task_graph.add_node(t['id'], pos=t['pos'], duration=t['duration'], robots=t['robots_needed'])
+            # add task nodes with attributes as node data
+            task_graph.add_node(t['id'], pos=t['pos'], duration=t['duration'], robots=t['robots_needed'],
+                                earliest_start=0)
 
             # Add edges from predecessors
             for p_id in t['predecessors']:
@@ -62,26 +67,22 @@ class Auctioneer(Node):
         except FileNotFoundError:
             print(f"Could not find tasks.json at {json_path}")
 
-    def visualize_task_graph(self, G):
+    def visualize_task_graph(self, graph):
         plt.figure(figsize=(8, 6))
 
-        pos = nx.spring_layout(G)
-        nx.draw_networkx_nodes(G, pos, node_size=700, node_color='skyblue')
-        nx.draw_networkx_edges(G, pos, arrowstyle='->', arrowsize=20, edge_color='gray')
-        nx.draw_networkx_labels(G, pos, font_size=12, font_family='sans-serif')
+        pos = nx.spring_layout(graph)
+        nx.draw_networkx_nodes(graph, pos, node_size=700, node_color='skyblue')
+        nx.draw_networkx_edges(graph, pos, arrowstyle='->', arrowsize=20, edge_color='gray')
+        nx.draw_networkx_labels(graph, pos, font_size=12, font_family='sans-serif')
 
         plt.title("Task Precedence Graph (DAG)")
         plt.axis('off')
         plt.show()
 
     def publish_free_tasks(self):
-        self.free_tasks = [(node, data) for node, data in self.task_graph.nodes(data=True) if self.task_graph.in_degree(node) == 0]
-    
-        """publishes all tasks which have no predecessor (ready to allocate)"""
-        
-        
-        print(type(self.free_tasks[0]))
+        """publishes"""
         msg = TaskList()
+        msg.auction_round = self.auction_round
 
         for t_id, t_data in self.free_tasks:
             t = Task()
@@ -89,12 +90,59 @@ class Auctioneer(Node):
             t.pos = [float(t_data['pos'][0]), float(t_data['pos'][1])]
             t.duration = t_data['duration']
             t.robots_needed = t_data['robots']
-            
+            t.earliest_start = t_data['earliest_start']
+
             # Append the individual task to the list
             msg.tasks.append(t)
-
+            self.free_tasks_bids[t_id] = None
         self.task_publisher.publish(msg)
 
+    def assign_best_bid(self):
+        """publishes a winner"""
+        best_task_id = min(self.free_tasks_bids,
+                           key=lambda k: self.free_tasks_bids[k][0] if self.free_tasks_bids[k] is not None else float(
+                               'inf'))
+
+        best_bid = Bid()
+        best_bid.task_id = best_task_id
+        best_bid.bid = self.free_tasks_bids[best_task_id][0]
+        best_bid.bidder_id = self.free_tasks_bids[best_task_id][1]
+        best_bid.auction_round = self.free_tasks_bids[best_task_id][2]
+        best_bid.task_end = self.free_tasks_bids[best_task_id][3]
+
+        self.winner_publisher.publish(best_bid)
+        self.allocated_tasks.append((best_task_id, self.task_graph.nodes[best_task_id]))
+        for succ in self.task_graph.successors(best_task_id):
+            current_earliest_start = self.task_graph.nodes[succ].get('earliest_start')
+            self.task_graph.nodes[succ]['earliest_start'] = max(current_earliest_start, best_bid.task_end)
+        self.task_graph.remove_node(best_task_id)
+
+    def bid_evaluator_callback(self, msg):
+        """evaluates incoming bids by saving the best ones in free_tasks_bids"""
+        bidder_id = msg.bidder_id
+        task_id = msg.task_id
+        bid_val = msg.bid
+        msg_round = msg.auction_round
+        task_end = msg.task_end
+
+        if msg_round == self.auction_round:
+            # only accepts current round bids
+            self.expected_bids -= 1
+
+            if self.free_tasks_bids[task_id] is None or self.free_tasks_bids[task_id][0] > bid:
+                self.free_tasks_bids[task_id] = (bid, bidder_id, auction_round, task_end)
+
+            if self.expected_bids == 0:
+                self.assign_best_bid()
+                self.start_next_auction()
+
+    def start_next_auction(self):
+        # odmah se računaju svi slobodni taskovi
+        self.free_tasks = [(node, data) for node, data in self.task_graph.nodes(data=True) if
+                               self.task_graph.in_degree(node) == 0]
+        self.expected_bids = self.get_num_robots()
+        self.auction_round += 1
+        self.free_tasks_bids.clear()
 
 
 def main(args=None):
@@ -103,6 +151,7 @@ def main(args=None):
     ros.spin(node)
     node.destroy_node()
     ros.shutdown()
+
 
 if __name__ == '__main__':
     main()
