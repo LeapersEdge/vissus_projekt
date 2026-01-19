5ffbdf68c945ddaa18116357b834f614962a9646
cicoita <itapoklepovic1@gmail.com>
2026-01-19 16:02:31 +0100

    comments on bidders

--- scripts/bidder.py
+++ scripts/bidder.py
@@ -57,7 +57,6 @@ class Bidder(Node):
 
     def closing_callback(self, msg):
         self.get_logger().info("Market closed... shutting down")
-        self.save_schedule()
         raise SystemExit
 
     def save_schedule(self):
@@ -137,7 +136,7 @@ class Bidder(Node):
         dist_from_robot = self.distance(self.start_position, t_obj.pos)
         arrival = dist_from_robot / self.speed
         earliest = max(arrival, t_obj.earliest_start)
-        graph.nodes[curr]['start_time'] = earliest if graph.nodes[curr]['start_time'] is None else max(graph.nodes[curr]['start_time'], earliest)
+        graph.nodes[curr]['start_time'] = earliest if graph.nodes[curr].get('start_time') is None else max(graph.nodes[curr]['start_time'], earliest)
 
         while True:
             if curr == new_node:
@@ -150,7 +149,8 @@ class Bidder(Node):
             dist = graph.edges[curr, nxt]['distance']
 
             arrival_next = graph.nodes[curr]['start_time'] + graph.nodes[curr]['task'].duration + (dist / self.speed)
-            graph.nodes[nxt]['start_time'] = max(arrival_next, graph.nodes[nxt]['task'].earliest_start)
+            earliest = max(arrival_next, graph.nodes[nxt]['task'].earliest_start)
+            graph.nodes[nxt]['start_time'] = earliest if graph.nodes[nxt].get('start_time') is None else max(earliest, graph.nodes[nxt]['start_time'])
 
             curr = nxt
 
